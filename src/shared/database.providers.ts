edcd71d6a5221bcc4cdabe95af14507dfcff2eb2
jiaqi <2468260248@qq.com>
2023-03-19 20:04:29 +0800

    ...

--- src/shared/database.providers.ts
+++ src/shared/database.providers.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from "@nestjs/config";
 import { DataSource, DataSourceOptions } from 'typeorm'
 import * as path from 'path'
+import { createConnection } from "typeorm";
 
 const databaseType: DataSourceOptions['type'] = 'mongodb'
 export const DatabaseProviders = [
@@ -8,17 +9,29 @@ export const DatabaseProviders = [
         provide: 'MONGODB_DATA_SOURCE',
         inject: [ConfigService], //url,用户名,密码
         useFactory: async (ConfigService: ConfigService) => {
+            //  createConnection({
+            //     type: databaseType,
+            //     host: "139.155.86.10",
+            //     port: 27017,
+            //     database: 'nest_server',
+            //     entities: [
+            //         path.join(__dirname, `../../**/*.mongo.entity{.ts,.js}`)
+            //     ],
+            // })
             const config = {
                 type: databaseType,
-                url: ConfigService.get<string>('database.url'),
-                username: ConfigService.get<string>('database.user'),
-                pasword: ConfigService.get<string>('database.pass'),
+                // host:"localhost",
+                host:"139.155.86.10",
+                // port:27017,
+                // url: ConfigService.get<string>('database.url'),
+                // username: ConfigService.get<string>('database.user'),
+                // pasword: ConfigService.get<string>('database.pass'),
                 database: ConfigService.get<string>('database.name'),
                 entities: [
                     path.join(__dirname, `../../**/*.mongo.entity{.ts,.js}`)
                 ],
-                logging: ConfigService.get<boolean>('database.logging'),
-                synchronize: ConfigService.get<boolean>('database.synchronize'),
+                // logging: ConfigService.get<boolean>('database.logging'),
+                // synchronize: ConfigService.get<boolean>('database.synchronize'),
 
             }
             console.log("===== config start ======\n", config);
@@ -27,6 +40,23 @@ export const DatabaseProviders = [
             const ds = new DataSource(config)
             await ds.initialize()
             return ds
+
+            // const AppDataSource = new DataSource({
+            //     type: databaseType,
+            //     host: "127.0.0.1",
+            //     port: 27017,
+            //     database: 'nest_server',
+            //     entities: [
+            //         path.join(__dirname, `../../**/*.mongo.entity{.ts,.js}`)
+            //     ],
+            // })
+
+            // await AppDataSource.initialize()
+            // console.log("Data Source has been initialized!")
+            // .catch((err) => {
+            //     console.error("Error during Data Source initialization", err)
+            // })
+            // return AppDataSource
         }
     }
 ]
