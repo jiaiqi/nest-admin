1bf325ac81b954833fa6f9f7fa8d7c0c97dc9928
jiaqi <2468260248@qq.com>
2023-04-04 16:40:51 +0800

    限制

---
+++ scripts/preinstall.js
@@ -0,0 +1,7 @@
+if (!/pnpm/.test(process.env.npm_execpath || '')) {
+  console.warn(
+//      `This repository requires using pnpm as the package manager for scripts to work properly`
+      `请使用pnpm作为包管理器`
+  )
+  process.exit(1)
+}
