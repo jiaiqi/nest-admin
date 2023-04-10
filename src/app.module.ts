0351c557a4de2ec71150620db783cfe2f648cb5f
jiaqi <2468260248@qq.com>
2023-04-10 23:02:36 +0800

    统一响应格式

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CMSModule } from './cms/cms.module';
+import { ResponseInterceptor } from '@/shared/interceptors/response.interceptors';
 
 @Module({
-  imports: [UserModule,CMSModule],
+  imports: [UserModule, CMSModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,{
+    provide: APP_INTERCEPTOR,
+    useClass: ResponseInterceptor,
+  },],
 })
-export class AppModule {}
+export class AppModule { }
+
